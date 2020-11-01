4a4a2cad802610989704f3c92f8a907bb456375b
DIYON7 <diyoners123@gmail.com>
2020-11-01 12:48:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Users>()
+    val listUsers = arrayOf(
+        "Alpha",
+        "Bravo",
+        "Diyon",
+        "Delta",
+        "Echo",
+        "Foxtrot",
+        "Golf",
+        "Hotel",
+        "Carena",
+        "Juliet"
+    )
+
+    val halo = arrayOf(
+        "Halo"
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        for (i in 0 until listUsers.size) {
+            list.add(Users(listUsers.get(i), halo.get(0)))
+            if (listUsers.size - 1 == i) {
+                val adapter = Adapter(list)
+                adapter.notifyDataSetChanged()
+                mRecyclerView.adapter = adapter
+            }
+        }
+    }
+}
